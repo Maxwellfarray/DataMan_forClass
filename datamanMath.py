bfda5de3e27317535370354d0af98ce1e5f38a9d
Maxwellfarray <136000185+Maxwellfarray@users.noreply.github.com>
2023-09-11 13:31:48 -0400

    Add files via upload

---
+++ datamanMath.py
@@ -0,0 +1,28 @@
+def mathParse(str):
+    b = 0
+    new = False
+    mathprep = ['']
+    for i in str:
+        print(i)
+        if i.isalpha():
+            print('please only enter math formula symbols and numbers')
+            break;
+        elif i.isnumeric():
+            if new == True:
+                b = b + 1
+                mathprep.append(i)
+            else:
+                mathprep[b] = mathprep[b] + i
+        elif (i=='+' or i=='-' or i== '/' or i=='*'):
+            b = b + 1
+            new = True
+            mathprep.append(i)
+    print(mathprep)        
+    print("end")
+
+
+egg = input()
+mathParse(egg)
+
+
+    
